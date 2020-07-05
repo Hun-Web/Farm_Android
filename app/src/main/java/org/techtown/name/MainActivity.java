f478ed6973144eccb14829cca6d78398357fb534
Seunghun Lee <seunghunlee@Seunghunui-MacBookPro.local>
2020-07-05 22:18:18 +0900

    Hello

---
+++ app/src/main/java/org/techtown/name/MainActivity.java
@@ -0,0 +1,32 @@
+package org.techtown.name;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.V;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onButton1Clicked(View v) {
+        Toast.makeText(this, "확인 1 버튼이 눌렸어요", Toast.LENGTH_LONG).show();
+    }
+    public void onButton2Clicked(View v) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://m.naver.com"));
+        startActivity(intent);
+    }
+    public void onButton3Clicked(View v) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:010-3071-9929"));
+        startActivity(intent);
+    }
+}
